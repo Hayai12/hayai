7f23f2296b55fa292969ab89570183d53bce2fca
L1E21 <L1E21@DESKTOP-PPCDS1D>
2022-02-10 12:14:40 -0500

    version 1

---
+++ src/hayai123/Hayai123.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hayai123;
+
+/**
+ *
+ * @author L1E21
+ */
+public class Hayai123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("ingrese un numero");
+    }
+    
+}
