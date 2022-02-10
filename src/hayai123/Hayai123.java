089146de9ce4b62e4be5411e008d8a82ede76504
L1E21 <L1E21@DESKTOP-PPCDS1D>
2022-02-10 12:32:34 -0500

    version 2

--- src/hayai123/Hayai123.java
+++ src/hayai123/Hayai123.java
@@ -16,7 +16,7 @@ public class Hayai123 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ingrese un numero");
+        System.out.println("Hola Mundo");
     }
     
 }
